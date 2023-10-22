0b21fb85bd2d1e15cd63cbaad7f20d6cb78204ba
MarcelPaluchGR4 <115399798+MarcelPaluchGR4@users.noreply.github.com>
2023-10-22 14:13:25 +0200

    zad 3.1

---
+++ zad3.1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string user_input;
+            double converted;
+            Console.WriteLine("Podaj rok:");
+            user_input = Console.ReadLine();
+            converted = int.Parse(user_input);
+            if (((converted % 4 == 0) && (converted % 100 != 0)) || (converted % 400 == 0))
+            {
+                Console.WriteLine("Rok przestepny");
+            }
+            else
+            {
+                Console.WriteLine("Rok nieprzestepny");
+            }
+                
+            ;
+        }
+    }
+}
